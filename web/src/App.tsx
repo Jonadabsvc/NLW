68ebe0ad6a020440dd5bf71b0babdf5e11a6140c
Jonadab <jonacerq@gmail.com>
2023-01-29 18:47:06 -0300

    finalização da landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import {MagnifyingGlassPlus} from "phosphor-react"
 import './styles/main.css';
 // Componentes / Propriedades
 
@@ -18,7 +19,7 @@ function App() {
           pb-4  é padding bottom 16 pixels, 16/4 = 4
           px-4  é padding lateral 16 pixels, 16/4 = 4
       */}
-       <div className="grid grid-cols-6 gap-6 mt-16"> {/* div dos jogos */}
+      <div className="grid grid-cols-6 gap-6 mt-16"> {/* div dos jogos */}
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-1.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
@@ -68,8 +69,18 @@ function App() {
         </a>
       </div>
 
-      <div className="bg-[#2A2634] px-8 py-6 my-8 self-stretch rounded-lg">
-        
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
       </div>
 
     </div>
