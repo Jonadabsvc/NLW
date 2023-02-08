db933ca1cfedd5677916690b26970d26262a59d9
Jonadab <jonacerq@gmail.com>
2023-02-07 23:46:21 -0300

    ligação do back-end com o front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import { MagnifyingGlassPlus } from "phosphor-react"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GameBanner } from "./components/GameBanner";
 import './styles/main.css';
 // Componentes / Propriedades
@@ -7,9 +7,27 @@ import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
 import { CreateAdBanner } from "./components/CreateAdBanner";
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
-  const [] = useState(false)
+  /*  a variavel games é um array de objetos
+    que tem o formato definido na interface Game*/
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
 
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
@@ -26,15 +44,18 @@ function App() {
           px-4  é padding lateral 16 pixels, 16/4 = 4
       */}
       <div className="grid grid-cols-6 gap-6 mt-16"> {/* div dos jogos */}
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={4} />
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={2} />
-        <GameBanner bannerUrl="/game-3.png" title="CS:GO" adsCount={5} />
-        <GameBanner bannerUrl="/game-4.png" title="Apex Legends" adsCount={3} />
-        <GameBanner bannerUrl="/game-5.png" title="Fortnite" adsCount={2} />
-        <GameBanner bannerUrl="/game-6.png" title="World of Warcraft" adsCount={2} />
+        {games.map(game => {
+          return (
+            <GameBanner
+            key={game.id}
+            bannerUrl={game.bannerUrl}
+            title={game.title}
+            adsCount={game._count.ads} />
+          )
+        })}
       </div>
 
-      <CreateAdBanner/>
+      <CreateAdBanner />
 
     </div>
   )
